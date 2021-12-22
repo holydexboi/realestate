23593d853228846914f376ae8df96038b088753a
holydexboi <holydexboi@yahoo.com>
2021-12-22 20:41:35 +0100

    first commit

---
+++ utils/fetchApi.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const baseUrl = 'https://bayut.p.rapidapi.com'
+
+
+export const fetchApi = async (url) => {
+    const { data } = await axios.get((url), {
+        headers: {
+            'x-rapidapi-host': 'bayut.p.rapidapi.com',
+            'x-rapidapi-key': 'd608096de3msh031fd473dce263cp159216jsn130a8896f9aa'
+        }
+    })
+
+    return data
+}
